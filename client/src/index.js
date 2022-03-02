838dab3e124725eafe801f87a3be01c2187f0861
Philip Lapinski <41872747+PolskaTag@users.noreply.github.com>
2022-03-02 15:09:50 -0500

    Basic styling for LandingMain

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
 import Discover from "./components/LandingPage/Discover";
 import Join from "./components/LandingPage/Join";
 import SignIn from "./components/LandingPage/SignIn";
